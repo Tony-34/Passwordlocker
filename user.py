ab8cfe80c1f0ad26c93f55ff628344555ddffc01
Tony <tr33947@gmail.com>
2020-11-20 20:41:11 +0300

    made changes

--- user.py
+++ user.py
@@ -14,3 +14,12 @@ class User:
         save_user method saves a new user objectto the user list
         '''
         User.save_userlist.append(self)
+
+
+    def delete_user(self):
+        User.save_userlist.remove(self)
+
+        @classmethod
+
+    def display_users(cls):
+        return cls.save_userlist
