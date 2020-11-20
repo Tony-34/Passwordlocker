bd2f9b60ddd433b79b76dfc93b07825397dac972
Tony <tr33947@gmail.com>
2020-11-20 20:30:08 +0300

    made changes

--- user.py
+++ user.py
@@ -9,4 +9,8 @@ class User:
         self.lname = lname
         self.email = email
 
-    def save_user (sefl):
+    def save_user (self):
+        '''
+        save_user method saves a new user objectto the user list
+        '''
+        User.save_userlist.append(self)
