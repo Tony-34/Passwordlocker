a36baf4002a415cc69b23175bf84e6ed6f97b74b
Tony <tr33947@gmail.com>
2020-11-20 20:23:59 +0300

    made changes

--- user.py
+++ user.py
@@ -2,4 +2,11 @@ class User:
     '''
     class  that generates instances for the user
     '''
-    
+    save_userlist = []
+    def _init_(self,fname,lname,email,):
+
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+
+    def save_user (sefl):
